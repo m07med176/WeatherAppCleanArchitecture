f39522c4d6d51038c14348d97c7e1e4182a89819
Mohamed Deiab <63822100+m07med176@users.noreply.github.com>
2023-03-07 03:21:43 +0200

    Add Loading state to seal DataResponseState.kt

--- app/src/main/java/iti/android/wheatherappjetpackcompose/domainLayer/usecase/home/GetWeatherDetailsUseCase.kt
+++ app/src/main/java/iti/android/wheatherappjetpackcompose/domainLayer/usecase/home/GetWeatherDetailsUseCase.kt
@@ -1,11 +1,10 @@
 package iti.android.wheatherappjetpackcompose.domainLayer.usecase.home
 
 import com.google.android.gms.maps.model.LatLng
-import iti.android.wheatherappjetpackcompose.dataLayer.source.dto.WeatherSuccessResponse
 import iti.android.wheatherappjetpackcompose.domainLayer.models.WeatherDetailsMapper
 import iti.android.wheatherappjetpackcompose.domainLayer.models.WeatherDetailsModel
 import iti.android.wheatherappjetpackcompose.domainLayer.repository.IMainRepository
-import iti.android.wheatherappjetpackcompose.utils.ApiResponseState
+import iti.android.wheatherappjetpackcompose.utils.DataResponseState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -15,7 +14,7 @@ import kotlinx.coroutines.launch
 class GetWeatherDetailsUseCase(private val repository: IMainRepository) {
 
     operator fun invoke(latLng: LatLng, units: String) =
-        flow<ApiResponseState<WeatherDetailsModel>> {
+        flow<DataResponseState<WeatherDetailsModel>> {
             CoroutineScope(Dispatchers.IO).launch {
                 val response = async {
                     repository.getWeatherDetails(
@@ -25,14 +24,18 @@ class GetWeatherDetailsUseCase(private val repository: IMainRepository) {
                     )
                 }
 
-                emit(ApiResponseState.OnLoading())
+                emit(DataResponseState.OnLoading())
                 if (response.await().isSuccessful) {
-                    val data = WeatherDetailsMapper().mapFromEntity(
-                        response.await().body() ?: WeatherSuccessResponse()
-                    )
-                    emit(ApiResponseState.OnSuccess<WeatherDetailsModel>(data))
+                    val responseData = response.await().body()
+                    if (responseData == null) {
+                        emit(DataResponseState.OnNothingData<WeatherDetailsModel>())
+                    } else {
+                        val data = WeatherDetailsMapper().mapFromEntity(responseData)
+                        emit(DataResponseState.OnSuccess<WeatherDetailsModel>(data))
+                    }
+
                 } else {
-                    emit(ApiResponseState.OnError<WeatherDetailsModel>(response.await().message()))
+                    emit(DataResponseState.OnError<WeatherDetailsModel>(response.await().message()))
                 }
             }
         }
