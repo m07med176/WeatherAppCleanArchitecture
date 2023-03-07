0a12632227b91e934f9c26c8b7bd3664fc14110b
Mohamed Deiab <63822100+m07med176@users.noreply.github.com>
2023-03-07 05:24:25 +0200

    ViewModel of AlertFragment.kt and Display states in UI

--- app/src/main/java/iti/android/wheatherappjetpackcompose/presentationLayer/ui/alert/AlertViewModel.kt
+++ app/src/main/java/iti/android/wheatherappjetpackcompose/presentationLayer/ui/alert/AlertViewModel.kt
@@ -1,7 +1,33 @@
 package iti.android.wheatherappjetpackcompose.presentationLayer.ui.alert
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import iti.android.wheatherappjetpackcompose.domainLayer.models.AlertModel
+import iti.android.wheatherappjetpackcompose.domainLayer.usecase.alert.AlertUseCases
+import iti.android.wheatherappjetpackcompose.utils.DataResponseState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
 
-class AlertViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AlertViewModel(private val useCases: AlertUseCases) : ViewModel() {
+    private val _state =
+        MutableStateFlow<DataResponseState<List<AlertModel>>>(DataResponseState.OnLoading())
+    val state: StateFlow<DataResponseState<List<AlertModel>>>
+        get() = _state
+
+
+    fun getAlertsList() {
+        viewModelScope.launch {
+            useCases.getAlert.invoke().catch {
+                _state.value = DataResponseState.OnError(it.message.toString())
+            }.collect { items ->
+                if (items.isEmpty())
+                    _state.value = DataResponseState.OnNothingData()
+                else
+                    _state.value = DataResponseState.OnSuccess(items)
+            }
+        }
+    }
+
 }
