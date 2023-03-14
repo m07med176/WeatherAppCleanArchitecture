dc995b067de71ada7ed98e9d9cc434f8708ab5f6
Mohamed Deiab <63822100+m07med176@users.noreply.github.com>
2023-03-14 19:20:51 +0200

    ALert Fix Bug

---
+++ app/src/main/java/iti/android/wheatherappjetpackcompose/dataLayer/source/dto/Alert.kt
@@ -0,0 +1,13 @@
+package iti.android.wheatherappjetpackcompose.dataLayer.source.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class Alert(
+    val description: String?,
+    val end: Long?,
+    val event: String?,
+    @SerializedName("sender_name")
+    val senderName: String?,
+    val start: Long?,
+    val tags: List<String> = emptyList(),
+)
