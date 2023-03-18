aec4723c638bcc34d2beda7f50366df7880425fb
Mohamed Deiab <63822100+m07med176@users.noreply.github.com>
2023-03-18 10:16:50 +0200

    fix bugs in github

---
+++ app/src/test/java/iti/android/wheatherappjetpackcompose/RemoteFakeDataSource.kt
@@ -0,0 +1,31 @@
+package iti.android.wheatherappjetpackcompose
+
+import com.google.android.gms.maps.model.LatLng
+import iti.android.wheatherappjetpackcompose.dataLayer.source.dto.WeatherSuccessResponse
+import iti.android.wheatherappjetpackcompose.dataLayer.source.remote.IRemoteDataSource
+import retrofit2.Response
+
+class RemoteFakeDataSource(
+    private var weatherSuccessResponse: WeatherSuccessResponse,
+) : IRemoteDataSource {
+    override suspend fun getWeatherDetails(
+        longitude: Double,
+        latitude: Double,
+        language: String,
+        units: String,
+    ): Response<WeatherSuccessResponse> {
+        return Response.success(weatherSuccessResponse)
+    }
+
+    override fun getCityName(lat: Double, long: Double): String {
+        return "Mansoura"
+    }
+
+    override fun getCityName(latLng: LatLng): String {
+        return "Mansoura"
+    }
+
+    override fun checkInternetConnectivity(): Boolean {
+        return true
+    }
+}
